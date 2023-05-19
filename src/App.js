aa9e8bd918601e78f4a0e214c2f124a8d9659c18
Hiteshwar <hemant.kaimwal96@gmail.com>
2023-05-19 09:58:28 +0530

    pass json data to Tour component and Tour component receive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import Tours from "./Tours";
+import data from './data';
 
 function App() {
+  const[tours,setTours]=useState(data); //tour variable get data, now tour is object
+
   return (
     <div className="App">
-      Hello
+      <h1>Plan With Love</h1>
+      <Tours tours={tours}/>
     </div>
   );
 }
---
+++ src/Tours.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from './Card';
+
+export default function Tours({tours}) {
+
+    // let {tour}=props.tour;
+
+  return (
+   <div className="container">
+        <div className='cards'>
+            // {
+            //   tours.map((tour) => {
+            //       return <Card {...tour}></Card> //passing tour object copy/cloning
+            //   })
+            // }
+      </div>
+   </div>
+  )
+}
